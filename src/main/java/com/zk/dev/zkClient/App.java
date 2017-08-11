f2a06f85c78d38d1456006096ea310782e6039cc
zhangliang24 <zhangliang24@baidu.com>
2017-08-11 15:43:01 +0800

    first commit

---
+++ src/main/java/com/zk/dev/zkClient/App.java
@@ -0,0 +1,17 @@
+package com.zk.dev.zkClient;
+
+import java.util.Calendar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+       // Calendar ca = null;
+        System.out.println(Calendar.getInstance().getTimeInMillis());
+    }
+}
